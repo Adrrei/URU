a781a07dcd2caf842fc8ac231e7c3d172ef337f3
Adrian Reithaug <ar+service@uru.no>
2019-08-12 22:26:18 +0200

    fix: ui and ux improvements
    add navigation bar
    optimize files (smaller images, less fonts, etc.)
    one united 'theme' for the site

---
+++ src/URU/wwwroot/js/layout.js
@@ -0,0 +1,9 @@
+﻿document.addEventListener('DOMContentLoaded', function () {
+    let toggleMenu = document.getElementById('x-fade');
+
+    toggleMenu.addEventListener('click', function () {
+        let dropdown = document.getElementsByClassName('dropdown')[0];
+        toggleMenu.classList.toggle('open');
+        dropdown.classList.toggle('mobile-nav-show');
+    });
+});
